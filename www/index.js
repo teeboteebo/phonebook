a56fa97b96238f807480abddc8c565555b84c36b
Jesper Asplund <jesper.asplund95@gmail.com>
2019-09-30 17:36:33 +0200

    fooling around with some styling - not worthy

--- www/index.js
+++ www/index.js
@@ -1,4 +1,13 @@
 let header = document.createElement('header');
-header.innerHTML = 'hej'
+header.innerHTML = 'Super duper telefonbok'
 header.setAttribute('class', 'header')
-document.body.append(header);
+
+let main = document.createElement('main');
+main.innerHTML = 'main sektion med kontaktinformation'
+main.setAttribute('class', 'contact-information')
+
+let aside = document.createElement('aside')
+aside.innerHTML = 'aside sektion med lista över alla kontakter'
+aside.setAttribute('class', 'contact-list')
+
+document.body.append(header, main, aside);
